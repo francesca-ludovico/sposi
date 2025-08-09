656e2998e8d3c4d77e93d70a9fcba2ffe4272375
francesca-ludovico <ludo.garin@gmail.com>
2025-08-09 22:29:59 +0200

    Update password and calendar event

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,20 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Matrimonio Francesca e Ludovico",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Sep 06, 2025 10:30'),
 
             // Event duration (IN MINUTES)
-            // duration: 120,
+            duration: 360,
 
-            // You can also choose to set an end time
-            // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Piazza Maria Ausiliatrice, 9, 10152 Torino TO',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            // description: "We can't wait to see you on our big day."
         }
     });
 
@@ -214,8 +211,7 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'fb8c725b3c4c464d008d4025f447004f') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
 	  $.ajax({
@@ -230,6 +226,7 @@ $(document).ready(function () {
                  error: function(xhr, status, error) {
                    // Handle any errors
                    console.error(error);
+                   $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
                  }
                }); 
   //          $.post('https://script.google.com/macros/s/AKfycbyT5GeDiD4iaqZMypeV5TSXMRrmpf-kPsWYFow3YXxdplHOO5I0ZQ3WW09hsiNa_IrX9Q/exec' , data)
